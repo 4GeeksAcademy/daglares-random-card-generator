6300fed11b6e3172f17fcfa2a57af5c4c6071d46
DAGarciaLopez <dagarcialopez5@gmail.com>
2025-01-11 17:39:00 +0000

    random card generator 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const primerPalo = document.querySelector("#paloArriba");
+  const segundoPalo = document.querySelector("#paloAbajo");
+  const valorCarta = document.querySelector("#valor");
+
+  const palos = ["♦", "♥", "♠", "♣"];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function generateRandomValue(lista) {
+    let randomValue = Math.floor(Math.random() * lista.length);
+    return lista[randomValue];
+  }
+
+  let randomValor = generateRandomValue(valores);
+  let randomPalo = generateRandomValue(palos);
+  primerPalo.innerHTML = randomPalo;
+  segundoPalo.innerHTML = randomPalo;
+  valorCarta.innerHTML = randomValor;
+
+  if (randomPalo == "♦" || randomPalo == "♥") {
+    primerPalo.style.color = "red";
+    segundoPalo.style.color = "red";
+  }
 };
